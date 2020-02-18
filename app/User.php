897e1e32c0078c0ae2172b1eb87d4c123f74da4c
GabMic <ruinsain@gmail.com>
2020-02-18 19:42:14 +0200

    wip

--- app/User.php
+++ app/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
 
     public function type()
     {
-        return $this->hasMany(Type::class);
+        return $this->hasMany(Type::class)->orderBy('name', 'desc');
     }
 
     public function budget()
